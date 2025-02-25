ec59e30b4c94045c39c6e646c706682d768e38f7
vaibhavIncedoAccount <pawar123rohan123@gmail.com>
2025-02-25 19:43:07 +0530

    added file1

---
+++ file1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int add_d(double a , double b)
+{
+  return a+b;
+}
+
+int sub_d(double a , double b)
+{
+  return a-b;
+}
+int main() {
+  printf("Hello World from account 2 !");
+  return 0;
+  
+}
